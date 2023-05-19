e70dfd6b5fd3c701e9739fcb3d1925362f0a4a6a
DanielCastillo2212 <danielandre.castillo@gmail.com>
2023-05-18 20:10:02 -0500

    Examen Semana 8

---
+++ Assets/Scripts/Enemy/EnemyController.cs
@@ -0,0 +1,67 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyController : MonoBehaviour
+{
+    private GameManager gm;
+    Rigidbody2D rb;
+    public GameObject gameManager;
+    // Start is called before the first frame update
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+
+    }
+
+    /*
+    void Update()
+    {
+        MatarZombie();
+    }
+    */
+
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        Debug.Log("Colision de bala");
+        if (other.gameObject.CompareTag("Bala"))
+        {
+          /*
+          gameManager = GameObject.Find("GameManager");
+          var gm = gameManager.GetComponent<GameManager>();
+          var uim = gameManager.GetComponent<UiManager>();
+
+          gm.PerderVidasZombie();
+
+          int morirZombie = gm.GetVidaZombie();
+        */
+
+          gameManager = GameObject.Find("GameManager");
+          var gm = gameManager.GetComponent<GameManager>();
+            Destroy(other.gameObject);
+            Destroy(this.gameObject);
+            GanarPuntaje();
+
+        }
+    }
+
+    private void GanarPuntaje(){
+      gameManager = GameObject.Find("GameManager");
+       var gm = gameManager.GetComponent<GameManager>();
+      gm.GanarMoneda();
+    }
+  
+   /*
+    private void MatarZombie(){
+
+      //(field) GameObject EnemyController.GameManager;
+      gameManager = GameObject.Find("GameManager");
+      var gm = gameManager.GetComponent<GameManager>();
+      var uim = gameManager.GetComponent<UiManager>();
+
+      gm.MatarZombie();
+      uim.PrintZombie(gm.GetZombies());
+    }
+    */
+}
